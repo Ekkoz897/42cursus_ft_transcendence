ce95304cdbc723b08ad012f68d072341e5a04d71
iuri <123178126+iurilandolt@users.noreply.github.com>
2025-01-08 16:47:59 +0000

    added container for postgres db : WIP

--- srcs/requirements/postgres/entry.sh
+++ srcs/requirements/postgres/entry.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-tail -f /dev/null
+if [ ! -f /var/lib/postgresql/data/init.sql ]; then
+    touch /var/lib/postgresql/data/init.sql
+    
+    postgres -D /var/lib/postgresql/data
+
+	psql -c "CREATE DATABASE $(cat /run/secrets/db_name);" 
+	psql -c "CREATE USER $(cat /run/secrets/db_user) WITH PASSWORD '$(cat /run/secrets/db_user_psw)';" 
+	psql -c "GRANT ALL PRIVILEGES ON DATABASE $(cat /run/secrets/db_name) TO $(cat /run/secrets/db_user);" 
+	psql -c "ALTER USER postgres WITH PASSWORD '$(cat /run/secrets/db_adm_psw)';"
+
+	pg_ctl -D /var/lib/postgresql/data stop
+
+fi
+
+
+
+postgres -D /var/lib/postgresql/data
