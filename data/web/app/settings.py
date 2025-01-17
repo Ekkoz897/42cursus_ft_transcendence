dae99bc687ba6c6006f4e6d8ef6e8c09839b6086
iuri <123178126+iurilandolt@users.noreply.github.com>
2025-01-17 19:23:07 +0000

    added async support for socket connections, added pong logic server side and input and rendering client side

--- data/web/app/settings.py
+++ data/web/app/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -28,17 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
-
 # Application definition
 
 INSTALLED_APPS = [
+	'daphne',
+	'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'channels',
 	'main',
 ]
 
@@ -122,7 +121,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -133,7 +131,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
--- data/web/main/consumers.py
+++ data/web/main/consumers.py
@@ -1,23 +1,26 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asyncio import sleep
 import json
-
+import logging
 class PongGameConsumer(AsyncWebsocketConsumer):
-	async def connect(self):
-		self.paddle_y = 250  # Initial paddle position
-		await self.accept()
+    async def connect(self):
+        self.paddle_y = 250
+        await self.accept()
+  
 
-	async def disconnect(self, close_code):
-		pass
+    async def disconnect(self, close_code):
+        pass
 
-	async def receive(self, text_data):
-		data = json.loads(text_data)
-		if data['action'] == 'move_paddle':
-			self.paddle_y = data['paddle_y']
-			# Send back the new position
-			await self.send(json.dumps({
-				'event': 'game_state',
-				'state': {
-					'paddle_y': self.paddle_y
-				}
-			}))
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+		# if data['action'] == 'heartbeat':
+		# 	await self.send(json.dumps({
+		# 		'event': 'heartbeat'
+		# 	}))
+        if data['action'] == 'move_paddle':
+            self.paddle_y = data['paddle_y']
+            await self.send(json.dumps({
+                'event': 'game_state',
+                'state': {
+                    'paddle_y': self.paddle_y
+                }
+            }))
