47e4c2f98b2df930873645005f1a060ab0303560
iuri <123178126+iurilandolt@users.noreply.github.com>
2025-01-27 16:44:56 +0000

    createGameField method is now part of the GameField Class, GameField html elements are created by the GameField class

--- data/web/static/js/SinglePong.js
+++ data/web/static/js/SinglePong.js
@@ -22,6 +22,32 @@ class Ball {
 	}
 }
 
+class ScoreBoard {
+	constructor(element) {
+		this.element = element;
+		
+		this.player1Info = element.querySelector("#player1-info");
+		this.player2Info = element.querySelector("#player2-info");
+
+		this.leftPlayerID = null;
+		this.rightPlayerID = null;
+
+		this.leftPlayerScore = 0;
+		this.rightPlayerScore = 0;
+	}
+	
+	update(leftScore, rightScore, leftID = this.leftPlayerID, rightID = this.rightPlayerID) {
+		this.leftPlayerScore = leftScore;
+		this.rightPlayerScore = rightScore;
+
+		this.leftPlayerID = leftID;
+		this.rightPlayerID = rightID;
+		
+		this.player1Info.textContent = `${this.leftPlayerID} : ${this.leftPlayerScore}`;
+		this.player2Info.textContent = `${this.rightPlayerScore} : ${this.rightPlayerID}`;
+	}
+}
+
 class GameField {
 	constructor(element) {
 		this.element = element;
@@ -32,52 +58,8 @@ class GameField {
 		this.element.style.width = `${w}px`;
 		this.element.style.height = `${h}px`;
 	}
-}
 
-class ScoreBoard {
-	constructor(element) {
-		this.element = element;
-		this.player1Info = element.querySelector("#player1-info");
-		this.player2Info = element.querySelector("#player2-info");
-		this.leftPlayerID = null;
-		this.leftPlayerScore = 0;
-		this.rightPlayerID = null;
-		this.rightPlayerScore = 0;
-	}
-
-	update(leftScore, rightScore, leftID, rightID) {
-		this.leftPlayerScore = leftScore;
-		this.rightPlayerScore = rightScore;
-		this.leftPlayerID = leftID;
-		this.rightPlayerID = rightID;
-
-		this.player1Info.textContent = `${this.leftPlayerID} : ${this.leftPlayerScore}`;
-		this.player2Info.textContent = `${this.rightPlayerScore} : ${this.rightPlayerID}`;
-	}
-}
-
-export class SinglePongPage extends BaseComponent {
-	constructor() {
-		super('static/html/singlepong.html');
-	}
-
-	async onIni() {
-		const element = this.getElementById("game-container");
-		if (element) {
-			await this.contentLoaded;
-
-			this.startButton = this.getElementById("start-button");
-			
-			this.startButton.addEventListener("click", () => {
-				this.startButton.classList.add('hidden');
-				this.createGameField();
-				this.startGame();
-			});
-
-		}
-	}
-
-	createGameField() {
+	static createElement(parent) {
 		const gameField = document.createElement('div');
 		gameField.id = 'game-field';
 		
@@ -98,11 +80,30 @@ export class SinglePongPage extends BaseComponent {
 		gameField.appendChild(paddle);
 		gameField.appendChild(ball);
 		
-		this.getElementById("game-container").appendChild(gameField);
+		parent.appendChild(gameField);
+		return new GameField(gameField);
+	}
+}
+
+export class SinglePongPage extends BaseComponent {
+	constructor() {
+		super('static/html/singlepong.html');
+	}
+
+	async onIni() {
+		const element = this.getElementById("game-container");
+		if (element) {
+			this.startButton = this.getElementById("start-button");
+			this.startButton.addEventListener("click", () => {
+				this.startButton.classList.add('hidden');
+				this.startGame();
+			});
+
+		}
 	}
 
 	async startGame() {
-		this.gameField = new GameField(this.getElementById("game-field"));
+		this.gameField = GameField.createElement(this.getElementById("game-container"));
 		this.paddle = new Paddle(this.getElementById("paddle"));
 		this.scoreBoard = new ScoreBoard(this.getElementById("score-board"));
 		this.ball = new Ball(this.getElementById("ball"));
@@ -110,19 +111,19 @@ export class SinglePongPage extends BaseComponent {
 
 		this.socket = new WebSocket(`ws://${window.location.host}/ws/pong/`);
 		this.socket.onopen = (event) => {
+			console.log(event);
 			this.socket.send(JSON.stringify({
 				action: "connect"
 			}));
-			console.log("on socket open triggered", event);
 		};
 
 		this.socket.onmessage = (event) => {
 			const data = JSON.parse(event.data);
 			const state = data.state;
-			// create separate init and update methods to use in game_state and game_start
+			
 			if (data.event === "game_state") {
 				this.paddle.update(state.paddle_y);
-				this.scoreBoard.update(state.player1_score, state.player2_score, this.scoreBoard.leftPlayerID, this.scoreBoard.rightPlayerID); 
+				this.scoreBoard.update(state.player1_score, state.player2_score); 
 				this.ball.update(state.ball_x, state.ball_y);
 			}
 			else if (data.event === "game_start") {
@@ -134,11 +135,11 @@ export class SinglePongPage extends BaseComponent {
 		};
 
 		this.socket.onclose = (event) => {
-			console.log("on socket close triggered", event);
+			console.log(event);
 		};
 
 		this.socket.onerror = (error) => {
-			console.log("on socket error triggered", error);
+			console.log(error);
 		};
 	}
 
@@ -161,7 +162,6 @@ export class SinglePongPage extends BaseComponent {
 	onDestroy() {
 		if (this.socket) {
 			this.socket.close();
-			console.log("on destroy called socket.close()");
 		}
 	}
 }
