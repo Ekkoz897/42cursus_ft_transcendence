d71b8886bac1392400f66a65b84f46e79230e8d1
iurilandolt <iurilandolt@gmail.com>
2025-03-28 19:05:18 +0000

    added push notifications for tournament progression when user is not on tournament page while enroled in a tournament

--- data/web/authservice/signals.py
+++ data/web/authservice/signals.py
@@ -1,15 +1,16 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver, Signal
 from backend.models import FriendshipRequest, User
+# from tournaments.models import Tournament
 from .consumers import LoginMenuConsumer
 import logging, asyncio
-from asgiref.sync import async_to_sync
+from channels.db import database_sync_to_async
 
 logger = logging.getLogger('pong')
 
 @receiver(post_delete, sender=FriendshipRequest)
 @receiver(post_save, sender=FriendshipRequest)
-def friendship_updated(sender, instance, **kwargs):
+async def friendship_updated(sender, instance, **kwargs):
 
 	if instance:
 		receiver = instance.receiver
@@ -17,13 +18,13 @@ def friendship_updated(sender, instance, **kwargs):
 	
 		for consumer in LoginMenuConsumer.instances:
 			if consumer.user.id == receiver_id:
-				async_to_sync(consumer.broadcast_notification)()
+				await consumer.broadcast_notification()
 				logger.debug(f"Sent friend update notification to {receiver.username}")
 				break
 
 		for consumer in LoginMenuConsumer.instances:
 			if consumer.user.id == instance.sender.id:
-				async_to_sync(consumer.broadcast_notification)()
+				await consumer.broadcast_notification()
 				logger.debug(f"Sent friend update notification to {instance.sender.username}")
 				break
 
@@ -38,4 +39,17 @@ async def profile_updated(sender, **kwargs):
 			logger.debug(f"Sent profile update notification to {user.username}")
 			break
 
+tournament_started_signal = Signal()
+tournament_updated_signal = Signal()
 
+@receiver(tournament_started_signal)
+@receiver(tournament_updated_signal)
+async def tournament_updated(sender, instance, **kwargs):
+	if instance: 
+		users = [await database_sync_to_async(User.objects.get)(username=username) for username in instance.players]
+		for user in users:
+			for consumer in LoginMenuConsumer.instances:
+				if consumer.user == user:
+					await consumer.broadcast({'event': 'tournament',})
+					logger.debug(f"Sent tournament update notification to {user.username}")
+					break
