160e7e5bb15b98730754428071506710c7c6061a
iurilandolt <iurilandolt@gmail.com>
2025-03-31 13:21:50 +0100

    post save and post delete linked signal function cant be async

--- data/web/authservice/signals.py
+++ data/web/authservice/signals.py
@@ -1,32 +1,30 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver, Signal
 from backend.models import FriendshipRequest, User
-# from tournaments.models import Tournament
 from .consumers import LoginMenuConsumer
 import logging, asyncio
 from channels.db import database_sync_to_async
+from asgiref.sync import async_to_sync
 
 logger = logging.getLogger('pong')
 
 @receiver(post_delete, sender=FriendshipRequest)
 @receiver(post_save, sender=FriendshipRequest)
-async def friendship_updated(sender, instance, **kwargs):
-
+def friendship_updated(sender, instance, **kwargs):
 	if instance:
 		receiver = instance.receiver
 		receiver_id = receiver.id
 	
 		for consumer in LoginMenuConsumer.instances:
 			if consumer.user.id == receiver_id:
-				await consumer.broadcast_notification()
-				logger.debug(f"Sent friend update notification to {receiver.username}")
+				async_to_sync(consumer.broadcast_notification)()
 				break
 
 		for consumer in LoginMenuConsumer.instances:
 			if consumer.user.id == instance.sender.id:
-				await consumer.broadcast_notification()
-				logger.debug(f"Sent friend update notification to {instance.sender.username}")
+				async_to_sync(consumer.broadcast_notification)()
 				break
+
 
 profile_updated_signal = Signal()
 
@@ -38,6 +36,7 @@ async def profile_updated(sender, **kwargs):
 			await consumer.broadcast_notification()
 			logger.debug(f"Sent profile update notification to {user.username}")
 			break
+
 
 tournament_started_signal = Signal()
 tournament_updated_signal = Signal()
